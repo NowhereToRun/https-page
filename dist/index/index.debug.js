123a32689dac6b0a6cf40b68b817e53991846d9e
NowhereToRun <zihao5@staff.sina.com.cn>
2017-10-19 14:43:34 +0800

    111

--- dist/index/index.debug.js
+++ dist/index/index.debug.js
@@ -2632,22 +2632,28 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 console.log(navigator);
 
 if ("geolocation" in navigator) {
+    // 第三个参数 https://developer.mozilla.org/zh-CN/docs/Web/API/PositionOptions
     navigator.geolocation.getCurrentPosition(function (position) {
+        console.log('new');
         console.log(position);
         // do_something(position.coords.latitude, position.coords.longitude);
     }, function (err) {
         console.log(err);
-    });
-
-    var watchID = navigator.geolocation.watchPosition(function (position) {
-        console.log(position);
-    }, function (err) {
-        console.log('in watch error');
     }, {
         enableHighAccuracy: true,
-        maximumAge: 5,
-        timeout: 27000
+        maximumAge: 0,
+        timeout: 20000
     });
+
+    // var watchID = navigator.geolocation.watchPosition(function (position) {
+    //     console.log(position);
+    // }, function (err) {
+    //     console.log('in watch error');
+    // }, {
+    //         enableHighAccuracy: true,
+    //         maximumAge: 0,
+    //         timeout: 27000
+    //     });
 } else {
     /* 地理位置服务不可用 */
     console.log('不可以;');
--- src/view/index/index.js
+++ src/view/index/index.js
@@ -3,22 +3,29 @@ import { $ } from '@mfelibs/base-utils'
 console.log(navigator)
 
 if ("geolocation" in navigator) {
+    // 第三个参数 https://developer.mozilla.org/zh-CN/docs/Web/API/PositionOptions
     navigator.geolocation.getCurrentPosition(function (position) {
+        console.log('new');
         console.log(position);
         // do_something(position.coords.latitude, position.coords.longitude);
     }, function (err) {
         console.log(err);
-    });
+    },{
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 20000
+    }
+);
 
-    var watchID = navigator.geolocation.watchPosition(function (position) {
-        console.log(position);
-    }, function (err) {
-        console.log('in watch error');
-    }, {
-            enableHighAccuracy: true,
-            maximumAge: 5,
-            timeout: 27000
-        });
+    // var watchID = navigator.geolocation.watchPosition(function (position) {
+    //     console.log(position);
+    // }, function (err) {
+    //     console.log('in watch error');
+    // }, {
+    //         enableHighAccuracy: true,
+    //         maximumAge: 0,
+    //         timeout: 27000
+    //     });
 } else {
     /* 地理位置服务不可用 */
     console.log('不可以;');
