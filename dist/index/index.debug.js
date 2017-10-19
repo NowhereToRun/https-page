3c69909b2322e5d3e8ca58dc1985dd01e94ccd77
NowhereToRun <zihao5@staff.sina.com.cn>
2017-10-19 14:56:19 +0800

    watch添加最大次数限制

--- dist/index/index.debug.js
+++ dist/index/index.debug.js
@@ -2633,6 +2633,7 @@ console.log(navigator);
 
 if ("geolocation" in navigator) {
     // 第三个参数 https://developer.mozilla.org/zh-CN/docs/Web/API/PositionOptions
+    // 只用getCurrentPosition 获取位置后手机上标志获取位置的箭头会消失
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log('new');
         console.log(position);
@@ -2645,15 +2646,20 @@ if ("geolocation" in navigator) {
         timeout: 20000
     });
 
-    // var watchID = navigator.geolocation.watchPosition(function (position) {
-    //     console.log(position);
-    // }, function (err) {
-    //     console.log('in watch error');
-    // }, {
-    //         enableHighAccuracy: true,
-    //         maximumAge: 0,
-    //         timeout: 27000
-    //     });
+    var times = 0;
+    var watchID = navigator.geolocation.watchPosition(function (position) {
+        console.log(position);
+        times = times + 1;
+        if (times === 6) {
+            navigator.geolocation.clearWatch(watchID);
+        }
+    }, function (err) {
+        console.log('in watch error');
+    }, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000
+    });
 } else {
     /* 地理位置服务不可用 */
     console.log('不可以;');
