70d454b772b006d8a336041ac65ffb8552fb092a
冯子豪 <zihao5@staff.sina.com.cn>
2017-10-19 10:51:44 +0800

    1

---
+++ src/view/index/index.js
@@ -0,0 +1,42 @@
+document.write("当你看到这句话 你的环境已经搭建成功");
+import "./js/webgl/tests/three/three.js"
+import "./js/webgl/tests/three/lighting.js"
+import "./js/webgl/tests/three/clipping.js"
+import "./js/webgl/tests/three/js/shaders/FresnelShader.js"
+import "./js/webgl/tests/three/js/loaders/DDSLoader.js"
+import "./js/webgl/tests/three/js/loaders/PVRLoader.js"
+import "./js/webgl/tests/three/bubbles.js"
+import "./js/webgl/tests/three/compressedTexture.js"
+import "./js/webgl/tests/three/js/Detector.js"
+
+import "./js/jquery.min.js"
+import "./js/detect-zoom.js"
+import "./js/languageDetector.js"
+import "./js/sha1.js"
+import "./js/helper.js"
+import "./js/webgl/webGL.js"
+
+
+
+import "./js/webgl/js/gl-matrix.js"
+import "./js/webgl/tests/no_texture.js"
+import "./js/webgl/tests/camera.js"
+import "./js/webgl/tests/lineTest.js"
+import "./js/webgl/tests/textureTest.js"
+import "./js/webgl/tests/simpleLightTest.js"
+import "./js/webgl/tests/moreLightTest.js"
+import "./js/webgl/tests/twoTexturesMoreLightTest.js"
+import "./js/webgl/tests/transparentTest.js"
+import "./js/webgl/tests/depth_texture/framework.js"
+import "./js/webgl/tests/depth_texture/meshes.js"
+import "./js/webgl/tests/depth_texture/webgl-texture-float-extension-shims.js"
+import "./js/webgl/tests/depth_texture/webgl-nuke-vendor-prefix.js"
+import "./js/webgl/tests/shadowTest.js"
+
+// import "http://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.3.10/seedrandom.min.js"
+// import "http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"
+
+
+
+import init from "./js/collector"
+init();
